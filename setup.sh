9911bf72d630e05d22d1bde7d2a36fb4d3df20b6
Jasper Van der Jeugt <m@jaspervdj.be>
2013-04-07 00:25:13 +0200

    Hmmk

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o nounset -o errexit -o pipefail
 
+# Utility: setup a single file
 function setup() {
     SRC="$1"
     DST="$2"
@@ -10,6 +11,7 @@ function setup() {
     ln -sfn "$PWD/$SRC" "$DST"
 }
 
+# Copy configuration files
 setup Xdefaults  "$HOME/.Xdefaults"
 setup ackrc      "$HOME/.ackrc"
 setup aliases    "$HOME/.aliases"
@@ -20,4 +22,16 @@ setup vimrc      "$HOME/.vimrc"
 setup xmonad.hs  "$HOME/.xmonad/xmonad.hs"
 setup zshrc      "$HOME/.zshrc"
 
+# Copy binaries
 setup bin/cabal-make "$HOME/.bin/cabal-make"
+
+# Create backup dir for vim
+echo 'Creating vim backup dir...'
+mkdir -p "$HOME/.vim/backup"
+
+# Install vundle if necessary
+echo 'Installing vundle...'
+if [[ ! -d "$HOME/.vim/bundle/vundle" ]]; then
+    git clone 'https://github.com/gmarik/vundle.git' "$HOME/.vim/bundle/vundle"
+    vim -c BundleInstall -c qa # Not sure if this is a nice way to do things
+fi
