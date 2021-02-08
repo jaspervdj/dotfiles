124c38b924a1fc6644fdd2b7eab22b64a12bb2d5
Jasper Van der Jeugt <m@jaspervdj.be>
2021-02-08 19:25:50 +0100

    Put nix| in front of nix prompt

--- config.fish
+++ config.fish
@@ -1,12 +1,10 @@
 . "$HOME/.aliases"
 
-function cdt
-    cd $argv
-    if [ (pwd) = "$HOME" ]
-        set title '~'
-    else
-        set title (basename (pwd))
-    end
-    echo "$title"
-    printf '\ek%s\e\\' "$title"
+functions -c fish_prompt _old_fish_prompt
+
+function fish_prompt
+  if test -n "$IN_NIX_SHELL"
+    echo -n "nix|"
+  end
+  _old_fish_prompt
 end
