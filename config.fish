709dfbced3f6d288b7a2af3005d3fa92aeefba32
Jasper Van der Jeugt <m@jaspervdj.be>
2024-05-29 18:26:34 +0200

    add envsource

--- config.fish
+++ config.fish
@@ -8,3 +8,11 @@ function fish_prompt
   end
   _old_fish_prompt
 end
+
+function envsource
+  for line in (grep -v '^#')
+    set item (string split -m 1 '=' $line)
+    set -gx $item[1] $item[2]
+    echo "$item[1]=$item[2]" >&2
+  end
+end
