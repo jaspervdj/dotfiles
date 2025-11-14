e7c554a8d6a9d325c2dde547d98f88a326d51bba
Jasper Van der Jeugt <m@jaspervdj.be>
2025-11-14 13:09:41 +0100

    Bump

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,5 @@
 {
   allowUnfree = true;
   allowBroken = true;
-  permittedInsecurePackages = [
-    "nodejs-16.20.2"
-    "nodejs-14.21.3"    # enso-security/connectors
-    "openssl-1.1.1u"    # enso-security/connectors
-    "openssl-1.1.1w"    # enso-security/shipper
-    "teleport-15.4.33"  # snyk
-  ];
+  permittedInsecurePackages = [];
 }
